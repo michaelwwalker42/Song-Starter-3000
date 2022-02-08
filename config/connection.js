e9a6476f3ac02d993250d69349da4aea6ce4a558
dmknapp2385 <dmknapp2385@gmail.com>
2022-02-08 13:26:45 -0700

    login page link added and login form basic

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+// import the Sequelize construcgtor from the library
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, 'root', 'dani7392385', {
+    dialect: 'mysql',
+    port: 3306
+  });
+}
+module.exports = sequelize;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 //get main page
 router.get('/', (req, res) => {
     res.render('homepage', {
-        loggedIn: true
+        loggedIn: false
     })
 });
 
