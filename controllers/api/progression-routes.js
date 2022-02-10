2b0dfaca5c5c9441ceda86294de9cd8b1d6d55a4
dmknapp2385 <dmknapp2385@gmail.com>
2022-02-09 19:45:09 -0700

    new routes

---
+++ controllers/api/progression-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+// create route to save progressions
+router.post('/', (req, res) => {
+
+});
+
+// create route to get all saved progressions 
+router.get('/', (req,res) => {
+
+});
+
+module.exports = router;
